2ab27936d9acb85a843d3e405adbd5c5f4207782
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2022-12-24 12:55:15 +0100

    Update haddock24.sh

--- haddock24.sh
+++ haddock24.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #===============================================================================
-# Configure python2.7
+# HADDOCK2.4 runs on python2.7, which is EOL.
+# This script is a workaround to run HADDOCK with a custom python2 installation
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-
 pyenv shell 2.7.18
-# python --version
 
 #===============================================================================
 # Configure HADDOCK2.4
@@ -14,5 +13,5 @@ export HADDOCK="$HOME/repos/haddock24"
 export HADDOCKTOOLS="$HADDOCK/tools"
 export PYTHONPATH="${PYTHONPATH}:$HADDOCK"
 
-python $HADDOCK/Haddock/RunHaddock.py
+python "$HADDOCK/Haddock/RunHaddock.py"
 #===============================================================================
