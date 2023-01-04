d7979c5f96ce1639b8af8c51af189992bfc344ea
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2023-01-04 10:36:26 +0100

    Update haddock3.sh
    Co-authored-by: Marco Giulini <54807167+mgiulini@users.noreply.github.com>

--- haddock3.sh
+++ haddock3.sh
@@ -5,7 +5,10 @@ HADDOCK3_DIR="$HOME/repos/haddock3"
 
 # shellcheck source=/dev/null
 source "$HADDOCK3_DIR/venv/bin/activate" || exit
-
+# if your haddock3 installation uses conda
+# conda init
+# source $HOME/anaconda3/bin/activate
+# source activate haddock3
 if [ $# -eq 0 ]; then
 	echo "No arguments supplied"
 	echo "Usage: haddock3.sh <input_file>"
