91e8df3c3b55dbcc0f24733783181b998422933f
Rodrigo V. Honorato <rvhonorato@protonmail.com>
2026-04-15 11:14:43 +0200

    add some colors

--- src/logging.rs
+++ src/logging.rs
@@ -5,10 +5,21 @@ pub fn init_logging(log_level: LevelFilter) {
     env_logger::Builder::from_default_env()
         .filter_level(log_level)
         .format(|buf, record| {
+            let level_style = match record.level() {
+                log::Level::Error => "\x1b[31m", // Red
+                log::Level::Warn => "\x1b[33m",  // Yellow
+                log::Level::Info => "\x1b[32m",  // Green
+                log::Level::Debug => "\x1b[36m", // Cyan
+                log::Level::Trace => "\x1b[35m", // Magenta
+            };
+            let reset = "\x1b[0m";
+            
             writeln!(
                 buf,
-                "[{}] {} - {}",
+                "{}[{}]{} {} - {}",
+                level_style,
                 record.level(),
+                reset,
                 record.target(),
                 record.args()
             )
